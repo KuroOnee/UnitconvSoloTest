95a1e73eba48c7fcbbc573a58596069fac6c9a82
KuroOnee <Dim22Rus@gmail.com>
2016-02-18 22:53:21 +0600

    Add main menu and choice

--- main.c
+++ main.c
@@ -3,9 +3,17 @@
 
 
 int main()
-{
+{	system("clear");
     setlocale(LC_ALL, "Rus");
     int choice;
-    printf("Добро пожаловать в программу Конвертер.");
+    printf("Добро пожаловать в программу Конвертер.\n\n");
+    printf("Выберите тип конвертируемых величин:\n");
+    printf("1. Длинна\n");
+    printf("2. Масса\n");
+    printf("3. Объем\n");
+    printf("4. Объем информации\n");
+    printf("5. Скорость\n\n");
+    printf("Введите номер пункта: ");
+    scanf("%d", &choice);
     return 0;
 }
